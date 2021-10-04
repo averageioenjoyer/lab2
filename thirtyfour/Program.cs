88c30b58ffb4bd8ab5e0c0faf561fcb2c98e0e6b
averageioenjoyer <91421527+averageioenjoyer@users.noreply.github.com>
2021-10-04 21:44:00 +0300

    Add files via upload

---
+++ thirtyfour/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace thirtyfour
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите m");
+            int m = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите g");
+            int g = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите a");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(m*g*Math.Cos(a));
+        }
+    }
+}
